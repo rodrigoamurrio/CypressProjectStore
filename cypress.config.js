6c2bd44cae4a56da23ff45f5656a3d65377ed41b
rodrigoamurrio <koky_ro@hotmail.com>
2025-11-22 17:39:23 -0400

    Configure reporter options in cypress.config.js
    Added reporter options for cypress-mochawesome-reporter.

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  
   reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+    charts: true
+  },
   env: {
     url: 'https://www.demoblaze.com/index.html'
   },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
     },
-
     specPattern: 'cypress/e2e/tests/*.js'
   },
 });
