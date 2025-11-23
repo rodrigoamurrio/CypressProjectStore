26afe16c4ed62ee84e9d73f7903b5c3d05d71a31
rodrigoamurrio <koky_ro@hotmail.com>
2025-11-22 21:18:51 -0400

    Refactor cypress.config.js for reporter options

--- cypress.config.js
+++ cypress.config.js
@@ -1,28 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  screenshotOnRunFailure:true,
-    video: true,
-    retries: 2,
-    reporterOptions: {
-        reportDir: 'cypress/reports/html',
-        overwrite: false,
-        html: true,
-        json: true,          // <-- Muy importante
-        charts: true,
-        inlineAssets: true,
-        saveAllAttempts: true 
-    },
-  env: {
-    url: 'https://www.demoblaze.com/index.html'
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports/html",
+    overwrite: false,
+    html: true,
+    json: true,
   },
+
   e2e: {
     setupNodeEvents(on, config) {
-      // Plugin de mochawesome
-      require('cypress-mochawesome-reporter/plugin')(on);
-      return config;
+      require("cypress-mochawesome-reporter/plugin")(on);
+      return config; // ← Esto es OBLIGATORIO
     },
-    specPattern: 'cypress/e2e/tests/*.js', // Asegúrate que tus tests estén aquí
-  }
+
+    specPattern: "cypress/e2e/tests/*.js",
+  },
 });
