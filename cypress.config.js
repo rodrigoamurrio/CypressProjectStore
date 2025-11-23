fecf44da85c8df2095e399efe17f3045db31abe9
rodrigoamurrio <koky_ro@hotmail.com>
2025-11-22 22:17:46 -0400

    Refactor Cypress configuration to use defineConfig

--- cypress.config.js
+++ cypress.config.js
@@ -1,65 +1,25 @@
-pipeline {
-    agent any
+const { defineConfig } = require("cypress");
 
-    tools {
-        nodejs "node25" // Nombre de tu NodeJS en Jenkins
-    }
-
-    stages {
-
-        stage('Checkout') {
-            steps {
-                checkout scm
-            }
-        }
-
-        stage('Preparar carpetas') {
-            steps {
-                bat """
-                mkdir cypress\\reports\\json || echo Carpeta JSON ya existe
-                mkdir cypress\\reports\\html || echo Carpeta HTML ya existe
-                """
-            }
-        }
-
-        stage('Instalar Dependencias') {
-            steps {
-                bat "npm install"
-            }
-        }
-
-        stage('Ejecutar Pruebas Cypress') {
-            steps {
-                bat """
-                npx cypress run --reporter mochawesome --reporter-options reportDir=cypress/reports/json,overwrite=false,html=false,json=true
-                """
-            }
-        }
-
-        stage('Merge JSON y Generar HTML') {
-            steps {
-                bat """
-                npx mochawesome-merge "cypress/reports/json/*.json" > "cypress/reports/json/report.json"
-                npx mochawesome-report-generator "cypress/reports/json/report.json" -o "cypress/reports/html"
-                """
-            }
-        }
-    }
-
-    post {
-        always {
-            // Archivar HTML como artefacto
-            archiveArtifacts artifacts: 'cypress/reports/html/**', fingerprint: true
-
-            // Publicar HTML usando plugin HTML Publisher
-            publishHTML([
-                allowMissing: false,
-                alwaysLinkToLastBuild: true,
-                keepAll: true,
-                reportDir: 'cypress/reports/html',
-                reportFiles: 'report.html',
-                reportName: 'Reporte Cypress Mochawesome'
-            ])
-        }
-    }
-}
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  screenshotOnRunFailure:true,
+    video: true,
+    retries: 2,
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: 'Cypress Test Report',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false
+    },
+  env: {
+    url: 'https://www.demoblaze.com/index.html'
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // Plugin de mochawesome
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+    specPattern: 'cypress/e2e/tests/*.js', // Asegúrate que tus tests estén aquí
+  }
+});
