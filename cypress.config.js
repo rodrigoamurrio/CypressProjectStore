e1d8c8959123566327279a98e343c72482b2d8e5
rodrigoamurrio <koky_ro@hotmail.com>
2025-11-22 22:09:31 -0400

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,65 @@
-const { defineConfig } = require("cypress");
+pipeline {
+    agent any
 
-module.exports = defineConfig({
-  reporter: "cypress-mochawesome-reporter",
-  reporterOptions: {
-    reportDir: "cypress/reports/html",
-    overwrite: false,
-    html: false,
-    json: true,
-  },
-    env: {
-    url: 'https://www.demoblaze.com/index.html'
-  },
+    tools {
+        nodejs "node25" // Nombre de tu NodeJS en Jenkins
+    }
 
-  e2e: {
-    setupNodeEvents(on, config) {
-      require("cypress-mochawesome-reporter/plugin")(on);
-      return config; // ← Esto es OBLIGATORIO
-    },
+    stages {
 
-    specPattern: "cypress/e2e/tests/*.js",
-  },
-});
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+
+        stage('Preparar carpetas') {
+            steps {
+                bat """
+                mkdir cypress\\reports\\json || echo Carpeta JSON ya existe
+                mkdir cypress\\reports\\html || echo Carpeta HTML ya existe
+                """
+            }
+        }
+
+        stage('Instalar Dependencias') {
+            steps {
+                bat "npm install"
+            }
+        }
+
+        stage('Ejecutar Pruebas Cypress') {
+            steps {
+                bat """
+                npx cypress run --reporter mochawesome --reporter-options reportDir=cypress/reports/json,overwrite=false,html=false,json=true
+                """
+            }
+        }
+
+        stage('Merge JSON y Generar HTML') {
+            steps {
+                bat """
+                npx mochawesome-merge "cypress/reports/json/*.json" > "cypress/reports/json/report.json"
+                npx mochawesome-report-generator "cypress/reports/json/report.json" -o "cypress/reports/html"
+                """
+            }
+        }
+    }
+
+    post {
+        always {
+            // Archivar HTML como artefacto
+            archiveArtifacts artifacts: 'cypress/reports/html/**', fingerprint: true
+
+            // Publicar HTML usando plugin HTML Publisher
+            publishHTML([
+                allowMissing: false,
+                alwaysLinkToLastBuild: true,
+                keepAll: true,
+                reportDir: 'cypress/reports/html',
+                reportFiles: 'report.html',
+                reportName: 'Reporte Cypress Mochawesome'
+            ])
+        }
+    }
+}
