ad4406b422438c86d743759133e87d5d0a2908bc
rodrigoamurrio <koky_ro@hotmail.com>
2025-11-24 00:04:50 -0400

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,16 +4,12 @@ module.exports = defineConfig({
   screenshotOnRunFailure: true,
   video: true,
   retries: 2,
-  reporter: 'mochawesome', // <-- cambiamos al mochawesome clásico
+   reporter: 'mochawesome',
   reporterOptions: {
-    reportDir: 'cypress/reports/html', // ruta donde se guardarán los reportes
-    overwrite: false,                  // no sobreescribe reportes anteriores
-    html: true,                        // genera HTML
-    json: true,                        // genera JSON (necesario para merge)
-    charts: true,
-    reportPageTitle: 'Cypress Test Report',
-    embeddedScreenshots: true,
-    inlineAssets: true,
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: false,
+    json: true,
   },
   env: {
     url: 'https://www.demoblaze.com/index.html'
